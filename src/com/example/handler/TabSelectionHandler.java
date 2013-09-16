445946b05e345f9339177803065cb98ad16ac17c
itechbear <itechbear@gmail.com>
2013-09-16 13:21:05 +0800

    add new branch 132.197 for idea v13 eap

--- src/com/example/handler/TabSelectionHandler.java
+++ src/com/example/handler/TabSelectionHandler.java
@@ -1,13 +1,10 @@
 package com.example.handler;
 
-import com.intellij.codeInsight.completion.CodeCompletionFeatures;
 import com.intellij.codeInsight.lookup.Lookup;
 import com.intellij.codeInsight.lookup.LookupManager;
-import com.intellij.codeInsight.lookup.impl.CompletionPreview;
 import com.intellij.codeInsight.lookup.impl.LookupImpl;
 import com.intellij.codeInsight.lookup.impl.actions.ChooseItemAction;
 import com.intellij.codeInsight.template.impl.TemplateSettings;
-import com.intellij.featureStatistics.FeatureUsageTracker;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
@@ -38,46 +35,37 @@ public class TabSelectionHandler extends EditorActionHandler {
             throw new AssertionError("The last lookup disposed at: " + LookupImpl.getLastLookupDisposeTrace() + "\n-----------------------\n");
         }
 
-        if (finishingChar == Lookup.NORMAL_SELECT_CHAR) {
-            if (!lookup.isFocused()) {
-                FeatureUsageTracker.getInstance().triggerFeatureUsed(CodeCompletionFeatures.EDITING_COMPLETION_CONTROL_ENTER);
-            }
-        } else if (finishingChar == Lookup.COMPLETE_STATEMENT_SELECT_CHAR) {
-            FeatureUsageTracker.getInstance().triggerFeatureUsed(CodeCompletionFeatures.EDITING_COMPLETION_FINISH_BY_SMART_ENTER);
-        } else if (finishingChar == Lookup.REPLACE_SELECT_CHAR) {
-            // Messages.showMessageDialog("tab key!", "title", Messages.getInformationIcon());
-            FeatureUsageTracker.getInstance().triggerFeatureUsed(CodeCompletionFeatures.EDITING_COMPLETION_REPLACE);
-
+        if (finishingChar == Lookup.REPLACE_SELECT_CHAR) {
             downSelect(lookup);
-            return;
-        } else if (finishingChar == '.') {
-            FeatureUsageTracker.getInstance().triggerFeatureUsed(CodeCompletionFeatures.EDITING_COMPLETION_FINISH_BY_CONTROL_DOT);
+        } else {
+            lookup.finishLookup(finishingChar);
         }
-
-        lookup.finishLookup(finishingChar);
     }
 
     @Override
     public boolean isEnabled(Editor editor, DataContext dataContext) {
         LookupImpl lookup = (LookupImpl) LookupManager.getActiveLookup(editor);
-        if (lookup == null) return false;
-        if (!lookup.isAvailableToUser()) return false;
-        if (focusedOnly && !CompletionPreview.hasPreview(lookup) && !lookup.isFocused()) return false;
+        if (lookup == null) {
+            return false;
+        }
+        if (!lookup.isAvailableToUser()) {
+            return false;
+        }
+        if (focusedOnly && lookup.getFocusDegree() == LookupImpl.FocusDegree.UNFOCUSED) {
+            return false;
+        }
         if (finishingChar == Lookup.NORMAL_SELECT_CHAR && ChooseItemAction.hasTemplatePrefix(lookup, TemplateSettings.ENTER_CHAR) ||
                 finishingChar == Lookup.REPLACE_SELECT_CHAR && ChooseItemAction.hasTemplatePrefix(lookup, TemplateSettings.TAB_CHAR)) {
             return false;
         }
         if (finishingChar == Lookup.REPLACE_SELECT_CHAR) {
-            if (lookup.isFocused()) {
-                return true;
-            }
             return !lookup.getItems().isEmpty();
         }
 
         return true;
     }
 
-    private void downSelect(LookupImpl lookup) {
+    private static void downSelect(LookupImpl lookup) {
         JList jList = lookup.getList();
         if (jList == null) {
             return;
@@ -91,7 +79,7 @@ public class TabSelectionHandler extends EditorActionHandler {
             return;
         }
         int index = (jList.getSelectedIndex() + 1) % count;
-        lookup.setFocused(true);
+        lookup.setFocusDegree(LookupImpl.FocusDegree.FOCUSED);
         jList.setSelectedIndex(index);
         int visible = index;
         if (visible > jList.getLastVisibleIndex() - 1) {
