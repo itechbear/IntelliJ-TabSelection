c20e1219836d3ed9dff87d9d891462846c720d7c
jerryouyang <jerryouyang@yeah.net>
2014-09-08 12:53:21 +0800

    add skipping function

--- src/com/github/itechbear/tabselection/component/TabSelectionComponent.java
+++ src/com/github/itechbear/tabselection/component/TabSelectionComponent.java
@@ -1,12 +1,13 @@
 package com.github.itechbear.tabselection.component;
 
+import com.github.itechbear.tabselection.handler.SkipParenthesisHandler;
 import com.github.itechbear.tabselection.handler.TabSelectionHandler;
 import com.intellij.codeInsight.lookup.Lookup;
 import com.intellij.codeInsight.lookup.impl.actions.ChooseItemAction;
-import com.intellij.codeInsight.template.impl.editorActions.ExpandLiveTemplateByTabAction;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.IdeActions;
 import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.editor.actions.TabAction;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -25,11 +26,14 @@ public class TabSelectionComponent implements ProjectComponent {
         // ActionManager.getInstance().addAnActionListener(new com.example.component.TabSelectionListener());
 
         // disable live template expansion key.
-        ExpandLiveTemplateByTabAction expandLiveTemplateByTabAction = (ExpandLiveTemplateByTabAction) ActionManager.getInstance().getAction(IdeActions.ACTION_EXPAND_LIVE_TEMPLATE_BY_TAB);
+        // ExpandLiveTemplateByTabAction expandLiveTemplateByTabAction = (ExpandLiveTemplateByTabAction) ActionManager.getInstance().getAction(IdeActions.ACTION_EXPAND_LIVE_TEMPLATE_BY_TAB);
         ActionManager.getInstance().unregisterAction(IdeActions.ACTION_EXPAND_LIVE_TEMPLATE_BY_TAB);
 
         ChooseItemAction.Replacing replacingAction = (ChooseItemAction.Replacing) ActionManager.getInstance().getAction(IdeActions.ACTION_CHOOSE_LOOKUP_ITEM_REPLACE);
         replacingAction.setupHandler(new TabSelectionHandler(false, Lookup.REPLACE_SELECT_CHAR));
+
+        TabAction tabAction = (TabAction) ActionManager.getInstance().getAction(IdeActions.ACTION_EDITOR_TAB);
+        tabAction.setupHandler(new SkipParenthesisHandler());
     }
 
     public void disposeComponent() {
---
+++ src/com/github/itechbear/tabselection/handler/SkipParenthesisHandler.java
@@ -0,0 +1,110 @@
+package com.github.itechbear.tabselection.handler;
+
+import com.intellij.codeStyle.CodeStyleFacade;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.editor.*;
+import com.intellij.openapi.editor.actionSystem.EditorActionManager;
+import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
+import com.intellij.openapi.editor.ex.EditorEx;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.ui.MacUIUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Created by HD on 2014/9/8.
+ */
+public class SkipParenthesisHandler extends EditorWriteActionHandler {
+    public SkipParenthesisHandler() {
+        super(true);
+    }
+
+    public void executeWriteAction(Editor editor, @Nullable Caret caret, DataContext dataContext) {
+        if(caret == null) {
+            caret = editor.getCaretModel().getPrimaryCaret();
+        }
+
+        CommandProcessor.getInstance().setCurrentCommandGroupId(Key.create("EditGroup"));
+        CommandProcessor.getInstance().setCurrentCommandName(EditorBundle.message("typing.command.name", new Object[0]));
+        Project project = (Project) CommonDataKeys.PROJECT.getData(dataContext);
+        if (skipParanthesis(editor, caret, project)) {
+            return;
+        }
+        insertTabAtCaret(editor, caret, project);
+    }
+
+    public boolean isEnabled(Editor editor, DataContext dataContext) {
+        return !editor.isOneLineMode() && !((EditorEx)editor).isEmbeddedIntoDialogWrapper() && !editor.isViewer();
+    }
+
+    private static boolean skipParanthesis(Editor editor, @NotNull Caret caret, Project project) {
+        if (caret.hasSelection()) {
+            return false;
+        }
+
+        int offset = editor.getCaretModel().getOffset();
+        Document document = editor.getDocument();
+        TextRange textRange = new TextRange(offset, offset + 1);
+        String string = document.getText(textRange);
+        if ( string.equals(")") || string.equals("}") || string.equals("]") ) {
+            editor.getCaretModel().moveToOffset(offset + 1);
+            return true;
+        }
+
+        return false;
+    }
+
+    private static void insertTabAtCaret(Editor editor, @NotNull Caret caret, Project project) {
+        MacUIUtil.hideCursor();
+        int columnNumber;
+        if(caret.hasSelection()) {
+            columnNumber = editor.visualToLogicalPosition(caret.getSelectionStartPosition()).column;
+        } else {
+            columnNumber = editor.getCaretModel().getLogicalPosition().column;
+        }
+
+        CodeStyleFacade settings = CodeStyleFacade.getInstance(project);
+        Document doc = editor.getDocument();
+        VirtualFile vFile = FileDocumentManager.getInstance().getFile(doc);
+        FileType fileType = vFile == null?null:vFile.getFileType();
+        int tabSize = settings.getIndentSize(fileType);
+        int spacesToAddCount = tabSize - columnNumber % Math.max(1, tabSize);
+        boolean useTab = editor.getSettings().isUseTabCharacter(project);
+        CharSequence chars = doc.getCharsSequence();
+        if(useTab && settings.isSmartTabs(fileType)) {
+            int e = editor.getCaretModel().getOffset();
+
+            while(e > 0) {
+                --e;
+                if(chars.charAt(e) != 9) {
+                    if(chars.charAt(e) != 10) {
+                        useTab = false;
+                    }
+                    break;
+                }
+            }
+        }
+
+        doc.startGuardedBlockChecking();
+
+        try {
+            if(useTab) {
+                EditorModificationUtil.typeInStringAtCaretHonorBlockSelection(editor, "\t", false);
+            } else {
+                EditorModificationUtil.typeInStringAtCaretHonorBlockSelection(editor, StringUtil.repeatSymbol(' ', spacesToAddCount), false);
+            }
+        } catch (ReadOnlyFragmentModificationException var16) {
+            EditorActionManager.getInstance().getReadonlyFragmentModificationHandler(doc).handle(var16);
+        } finally {
+            doc.stopGuardedBlockChecking();
+        }
+    }
+}
