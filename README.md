b669296b9eb07f59a9adf940b2477283275aa5e6
itechbear <itechbear@gmail.com>
2020-04-08 21:40:21 +0800

    update for 2019.3

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 IntelliJ-TabSelection
 =====================
 
@@ -31,3 +32,38 @@ Installation
  - Open this project and compile
  - Click "Build" menu and select "Prepare Plugin Module Intellij-TabSelection For Deployment"
  - Install the generated .jar file in the "Settings -> Plugins" dialog.
+=======
+IntelliJ-TabSelection
+=====================
+
+An intellij idea plugin that enables tab key to choose items from the suggest list.
+
+Features
+=======
+
+ - You can use tab key to down select items like the down arrow key in the suggest list.
+ - And ... it disables default tab key behavior after the suggest list popups up.
+
+Settings
+========
+
+This plugin doesn't provide any settings.
+
+Compatibility
+=============
+
+Tested on idea-IC 135
+
+Caution
+=======
+
+This plugin disables tab key expansion of live templates.
+
+Installation
+============
+
+ - Please follow [PluginDevelopment](http://confluence.jetbrains.com/display/IDEADEV/PluginDevelopment) to setup up your IDE
+ - Open this project and compile
+ - Click "Build" menu and select "Prepare Plugin Module Intellij-TabSelection For Deployment"
+ - Install the generated .jar file in the "Settings -> Plugins" dialog.
+>>>>>>> update for 2019.3
--- src/main/java/com/github/itechbear/tabselection/component/TabSelectionComponent.java
+++ src/main/java/com/github/itechbear/tabselection/component/TabSelectionComponent.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.github.itechbear.tabselection.component;
 
 import com.github.itechbear.tabselection.handler.TabSelectionHandler;
@@ -43,3 +44,50 @@ public class TabSelectionComponent implements ProjectComponent {
 
     }
 }
+=======
+package com.github.itechbear.tabselection.component;
+
+import com.github.itechbear.tabselection.handler.TabSelectionHandler;
+import com.intellij.codeInsight.lookup.Lookup;
+import com.intellij.codeInsight.lookup.impl.actions.ChooseItemAction;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.IdeActions;
+import com.intellij.openapi.components.ProjectComponent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: HD
+ * Date: 13-8-26
+ * Time: 12:28AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TabSelectionComponent implements ProjectComponent {
+    public TabSelectionComponent() {
+    }
+
+    public void initComponent() {
+        // TODO: insert component initialization logic here
+        ChooseItemAction.Replacing replacingAction = (ChooseItemAction.Replacing) ActionManager.getInstance().getAction(IdeActions.ACTION_CHOOSE_LOOKUP_ITEM_REPLACE);
+        replacingAction.setupHandler(new TabSelectionHandler(false, Lookup.REPLACE_SELECT_CHAR));
+    }
+
+    public void disposeComponent() {
+        // TODO: insert component disposal logic here
+    }
+
+    @NotNull
+    public String getComponentName() {
+        return "TabSelectionComponent";
+    }
+
+    @Override
+    public void projectOpened() {
+    }
+
+    @Override
+    public void projectClosed() {
+
+    }
+}
+>>>>>>> update for 2019.3
