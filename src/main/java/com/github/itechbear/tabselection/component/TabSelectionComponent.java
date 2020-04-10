ddf042ecd203f6fdd27321e12deaa9df1fb8bfb3
itechbear <itechbear@gmail.com>
2020-04-10 09:37:37 +0800

    Compatible with IDEA 2020.1

--- src/main/java/com/github/itechbear/tabselection/component/TabSelectionComponent.java
+++ src/main/java/com/github/itechbear/tabselection/component/TabSelectionComponent.java
@@ -1,13 +1,14 @@
-<<<<<<< HEAD
 package com.github.itechbear.tabselection.component;
 
 import com.github.itechbear.tabselection.handler.TabSelectionHandler;
 import com.intellij.codeInsight.lookup.Lookup;
 import com.intellij.codeInsight.lookup.impl.actions.ChooseItemAction;
+import com.intellij.ide.AppLifecycleListener;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.IdeActions;
-import com.intellij.openapi.components.ProjectComponent;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,78 +17,10 @@ import org.jetbrains.annotations.NotNull;
  * Time: 12:28AM
  * To change this template use File | Settings | File Templates.
  */
-public class TabSelectionComponent implements ProjectComponent {
-    public TabSelectionComponent() {
-    }
-
-    public void initComponent() {
-        // TODO: insert component initialization logic here
+public class TabSelectionComponent implements AppLifecycleListener {
+    @Override
+    public void appFrameCreated(@NotNull List<String> commandLineArgs) {
         ChooseItemAction.Replacing replacingAction = (ChooseItemAction.Replacing) ActionManager.getInstance().getAction(IdeActions.ACTION_CHOOSE_LOOKUP_ITEM_REPLACE);
         replacingAction.setupHandler(new TabSelectionHandler(false, Lookup.REPLACE_SELECT_CHAR));
     }
-
-    public void disposeComponent() {
-        // TODO: insert component disposal logic here
-    }
-
-    @NotNull
-    public String getComponentName() {
-        return "TabSelectionComponent";
-    }
-
-    @Override
-    public void projectOpened() {
-    }
-
-    @Override
-    public void projectClosed() {
-
-    }
 }
-=======
-package com.github.itechbear.tabselection.component;
-
-import com.github.itechbear.tabselection.handler.TabSelectionHandler;
-import com.intellij.codeInsight.lookup.Lookup;
-import com.intellij.codeInsight.lookup.impl.actions.ChooseItemAction;
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.IdeActions;
-import com.intellij.openapi.components.ProjectComponent;
-import org.jetbrains.annotations.NotNull;
-
-/**
- * Created with IntelliJ IDEA.
- * User: HD
- * Date: 13-8-26
- * Time: 12:28AM
- * To change this template use File | Settings | File Templates.
- */
-public class TabSelectionComponent implements ProjectComponent {
-    public TabSelectionComponent() {
-    }
-
-    public void initComponent() {
-        // TODO: insert component initialization logic here
-        ChooseItemAction.Replacing replacingAction = (ChooseItemAction.Replacing) ActionManager.getInstance().getAction(IdeActions.ACTION_CHOOSE_LOOKUP_ITEM_REPLACE);
-        replacingAction.setupHandler(new TabSelectionHandler(false, Lookup.REPLACE_SELECT_CHAR));
-    }
-
-    public void disposeComponent() {
-        // TODO: insert component disposal logic here
-    }
-
-    @NotNull
-    public String getComponentName() {
-        return "TabSelectionComponent";
-    }
-
-    @Override
-    public void projectOpened() {
-    }
-
-    @Override
-    public void projectClosed() {
-
-    }
-}
->>>>>>> update for 2019.3
--- src/main/java/com/github/itechbear/tabselection/handler/TabSelectionHandler.java
+++ src/main/java/com/github/itechbear/tabselection/handler/TabSelectionHandler.java
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
 package com.github.itechbear.tabselection.handler;
 
 import com.intellij.codeInsight.lookup.Lookup;
+import com.intellij.codeInsight.lookup.LookupFocusDegree;
 import com.intellij.codeInsight.lookup.LookupManager;
 import com.intellij.codeInsight.lookup.impl.LookupImpl;
-import com.intellij.codeInsight.lookup.impl.actions.ChooseItemAction;
-import com.intellij.codeInsight.template.impl.TemplateSettings;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
@@ -31,20 +30,6 @@ public class TabSelectionHandler extends EditorActionHandler {
         this.finishingChar = finishingChar;
     }
 
-//    @Override
-//    public void execute(@NotNull final Editor editor, final DataContext dataContext) {
-//        final LookupImpl lookup = (LookupImpl) LookupManager.getActiveLookup(editor);
-//        if (lookup == null) {
-//            throw new AssertionError("The last lookup disposed at: " + LookupImpl.getLastLookupDisposeTrace() + "\n-----------------------\n");
-//        }
-//
-//        if (finishingChar == Lookup.REPLACE_SELECT_CHAR) {
-//            downSelect(lookup);
-//        } else {
-//            lookup.finishLookup(finishingChar);
-//        }
-//    }
-
     @Override
     protected void doExecute(Editor editor, @Nullable Caret caret, DataContext dataContext) {
         // super.doExecute(editor, caret, dataContext);
@@ -61,21 +46,11 @@ public class TabSelectionHandler extends EditorActionHandler {
     }
 
     @Override
-    public boolean isEnabled(Editor editor, DataContext dataContext) {
-        LookupImpl lookup = (LookupImpl) LookupManager.getActiveLookup(editor);
-        if (lookup == null) {
-            return false;
-        }
-        if (!lookup.isAvailableToUser()) {
-            return false;
-        }
-        if (focusedOnly && lookup.getFocusDegree() == LookupImpl.FocusDegree.UNFOCUSED) {
-            return false;
-        }
-        if (finishingChar == Lookup.NORMAL_SELECT_CHAR && ChooseItemAction.hasTemplatePrefix(lookup, TemplateSettings.ENTER_CHAR) ||
-                finishingChar == Lookup.REPLACE_SELECT_CHAR && ChooseItemAction.hasTemplatePrefix(lookup, TemplateSettings.TAB_CHAR)) {
-            return false;
-        }
+    public boolean isEnabledForCaret(@NotNull Editor editor, @NotNull Caret caret, DataContext dataContext) {
+        LookupImpl lookup = (LookupImpl)LookupManager.getActiveLookup(editor);
+        if (lookup == null) return false;
+        if (!lookup.isAvailableToUser()) return false;
+        if (focusedOnly && lookup.getLookupFocusDegree() == LookupFocusDegree.UNFOCUSED) return false;
         if (finishingChar == Lookup.REPLACE_SELECT_CHAR) {
             return !lookup.getItems().isEmpty();
         }
@@ -97,9 +72,8 @@ public class TabSelectionHandler extends EditorActionHandler {
             return;
         }
         int index = (jList.getSelectedIndex() + 1) % count;
-        lookup.setFocusDegree(LookupImpl.FocusDegree.FOCUSED);
+        lookup.setLookupFocusDegree(LookupFocusDegree.FOCUSED);
         jList.setSelectedIndex(index);
-//        jList.ensureIndexIsVisible(index);
         int last = index + jList.getVisibleRowCount() / 2;
         Rectangle rectangle = jList.getCellBounds(index, last);
         if (rectangle == null) {
@@ -108,115 +82,4 @@ public class TabSelectionHandler extends EditorActionHandler {
         jList.scrollRectToVisible(rectangle);
     }
 
-=======
-package com.github.itechbear.tabselection.handler;
-
-import com.intellij.codeInsight.lookup.Lookup;
-import com.intellij.codeInsight.lookup.LookupManager;
-import com.intellij.codeInsight.lookup.impl.LookupImpl;
-import com.intellij.codeInsight.lookup.impl.actions.ChooseItemAction;
-import com.intellij.codeInsight.template.impl.TemplateSettings;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
-import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
-import java.awt.*;
-
-/**
- * Created with IntelliJ IDEA.
- * User: HD
- * Date: 13-8-26
- * Time: 12:15AM
- * To change this template use File | Settings | File Templates.
- */
-public class TabSelectionHandler extends EditorActionHandler {
-    final boolean focusedOnly;
-    final char finishingChar;
-
-    public TabSelectionHandler(boolean focusedOnly, char finishingChar) {
-        this.focusedOnly = focusedOnly;
-        this.finishingChar = finishingChar;
-    }
-
-//    @Override
-//    public void execute(@NotNull final Editor editor, final DataContext dataContext) {
-//        final LookupImpl lookup = (LookupImpl) LookupManager.getActiveLookup(editor);
-//        if (lookup == null) {
-//            throw new AssertionError("The last lookup disposed at: " + LookupImpl.getLastLookupDisposeTrace() + "\n-----------------------\n");
-//        }
-//
-//        if (finishingChar == Lookup.REPLACE_SELECT_CHAR) {
-//            downSelect(lookup);
-//        } else {
-//            lookup.finishLookup(finishingChar);
-//        }
-//    }
-
-    @Override
-    protected void doExecute(Editor editor, @Nullable Caret caret, DataContext dataContext) {
-        // super.doExecute(editor, caret, dataContext);
-        final LookupImpl lookup = (LookupImpl) LookupManager.getActiveLookup(editor);
-        if (lookup == null) {
-            throw new AssertionError("The last lookup disposed at: " + LookupImpl.getLastLookupDisposeTrace() + "\n-----------------------\n");
-        }
-
-        if (finishingChar == Lookup.REPLACE_SELECT_CHAR) {
-            downSelect(lookup);
-        } else {
-            lookup.finishLookup(finishingChar);
-        }
-    }
-
-    @Override
-    public boolean isEnabled(Editor editor, DataContext dataContext) {
-        LookupImpl lookup = (LookupImpl) LookupManager.getActiveLookup(editor);
-        if (lookup == null) {
-            return false;
-        }
-        if (!lookup.isAvailableToUser()) {
-            return false;
-        }
-        if (focusedOnly && lookup.getFocusDegree() == LookupImpl.FocusDegree.UNFOCUSED) {
-            return false;
-        }
-        if (finishingChar == Lookup.NORMAL_SELECT_CHAR && ChooseItemAction.hasTemplatePrefix(lookup, TemplateSettings.ENTER_CHAR) ||
-                finishingChar == Lookup.REPLACE_SELECT_CHAR && ChooseItemAction.hasTemplatePrefix(lookup, TemplateSettings.TAB_CHAR)) {
-            return false;
-        }
-        if (finishingChar == Lookup.REPLACE_SELECT_CHAR) {
-            return !lookup.getItems().isEmpty();
-        }
-
-        return true;
-    }
-
-    private static void downSelect(LookupImpl lookup) {
-        JList jList = lookup.getList();
-        if (jList == null) {
-            return;
-        }
-        ListModel listModel = jList.getModel();
-        if (listModel == null) {
-            return;
-        }
-        int count = listModel.getSize();
-        if (count == 0) {
-            return;
-        }
-        int index = (jList.getSelectedIndex() + 1) % count;
-        lookup.setFocusDegree(LookupImpl.FocusDegree.FOCUSED);
-        jList.setSelectedIndex(index);
-//        jList.ensureIndexIsVisible(index);
-        int last = index + jList.getVisibleRowCount() / 2;
-        Rectangle rectangle = jList.getCellBounds(index, last);
-        if (rectangle == null) {
-            return;
-        }
-        jList.scrollRectToVisible(rectangle);
-    }
-
->>>>>>> update for 2019.3
 }
